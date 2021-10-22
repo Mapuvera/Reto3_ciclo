31a399effed63f65692baeba0114525238d30c2d
Paula <maris.vera.mt@correo.usa.edu.co>
2021-10-22 11:16:29 -0500

    Reto 3 final

---
+++ src/main/java/co/usa/ciclo3/ciclo3/Ciclo3Application.java
@@ -0,0 +1,22 @@
+/*
+1. Modelo
+2. Interface
+3. Repositorio
+4. Servicios
+5. Controlador
+ */
+
+
+package co.usa.ciclo3.ciclo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ciclo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ciclo3Application.class, args);
+	}
+
+}
